825db30dfce97321aa0c9c7697eee6956249ab4e
Derek Adams <derek.adams@reveal-tech.com>
2013-10-07 16:19:38 -0400

    Updated example to include sending measurements on an interval and alerts at the press of a button.

--- SiteWhere/examples/send_events/send_events.ino
+++ SiteWhere/examples/send_events/send_events.ino
@@ -1,38 +1,97 @@
 #include <SPI.h>
-#include <Ethernet.h>
-#include <SiteWhere.h>
 #include <PubSubClient.h>
+#include <Ethernet.h>
+#include <SiteWhereEvents.h>
+#include <SiteWhere.h>
 
+/**
+ * Send message to SiteWhere when a button is pressed.
+ */
+
+/** Constants */
+const int BUTTON_PIN = 2;
+const int LED_PIN =  13;
+const int HEART_RATE_SEND_INTERVAL = 10000;
+const int NURSE_CALL_MIN_INTERVAL = 1000;
+
+/** Global variables */
+int buttonState = 0;
+long lastMeasurementTime = -HEART_RATE_SEND_INTERVAL;
+long lastAlertTime = -NURSE_CALL_MIN_INTERVAL;
+
+/** Local MAC/IP and MQTT IP */
 byte mac[]    = { 0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
 byte ip[]     = { 192, 168, 1, 20 };
 byte mqtt[]   = { 192, 168, 1, 68 };
 
+/** Variables for simulating heart rate */
+const int MIN_HEART_RATE = 60;
+const int MAX_HEART_RATE = 170;
+int delta = 5;
+int currentHeartRate = MIN_HEART_RATE;
+
+/** Buffer for JSON message */
+char messageBuffer[250];
+
+/** Callback function header */
+void callback(char* topic, byte* payload, unsigned int length);
+
 EthernetClient ethClient;
-SiteWhere sitewhere(ethClient, mqtt, 1883);
-DeviceAlert alert("fire.alarm", "The house is on fire!", "2013-10-03T15:15:12Z");
-DeviceLocation location(34.12377073007217, -84.19863080926007, 0, "2013-10-03T15:15:12Z");
-DeviceMeasurement measurement("heart.rate", "100", "2013-10-03T15:15:12Z");
+PubSubClient mqttClient(mqtt, 1883, callback, ethClient);
+SiteWhere sitewhere;
+
+SiteWhereMeasurement measurement("heart.rate", "100", "2013-10-03T15:15:12Z");
+SiteWhereAlert alert("nurse.call", "Nurse call button pressed!", "2013-10-03T15:15:12Z");
 
 void setup() {
+  pinMode(LED_PIN, OUTPUT);      
+  pinMode(BUTTON_PIN, INPUT);
+  
   Serial.begin(9600);
   Ethernet.begin(mac, ip);
-  Serial.println("Ethernet started. Attempting connect...");
-  if (sitewhere.connect("arduinoClient")) {
-    Serial.println("Connected!");
-    if (sitewhere.sendDeviceAlert("SiteWhere/input", "2394738380-MEI-001", alert, "SiteWhere/response")) {
-      Serial.println("Sent alert!");
-    }
-    if (sitewhere.sendDeviceLocation("SiteWhere/input", "2394738380-MEI-001", location, "SiteWhere/response")) {
-      Serial.println("Sent location!");
-    }
-    if (sitewhere.sendDeviceMeasurement("SiteWhere/input", "2394738380-MEI-001", measurement, "SiteWhere/response")) {
-      Serial.println("Sent measurement!");
+  if (mqttClient.connect("arduinoClient")) {
+    Serial.println(F("Connected to MQTT."));
+    if (!mqttClient.subscribe("SiteWhere/response")) {
+      Serial.println(F("Failed on subscribe to response topic."));
     }
   } else {
-    Serial.println("Connected failed.");
+    Serial.println(F("Unable to connect to SiteWhere via MQTT."));
   }
 }
 
 void loop() {
-  sitewhere.loop();
+  buttonState = digitalRead(BUTTON_PIN);
+  
+  if ((millis() - lastMeasurementTime) > HEART_RATE_SEND_INTERVAL) {
+    lastMeasurementTime = millis();
+    sitewhere.createMessage(messageBuffer, "2394738380-MEI-001", measurement, "SiteWhere/response");
+    mqttClient.publish("SiteWhere/input", messageBuffer);
+  }
+
+  if ((buttonState == HIGH) && ((millis() - lastAlertTime) > NURSE_CALL_MIN_INTERVAL)) {
+    lastAlertTime = millis();
+    sitewhere.createMessage(messageBuffer, "2394738380-MEI-001", alert, "SiteWhere/response");
+    mqttClient.publish("SiteWhere/input", messageBuffer);
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    digitalWrite(LED_PIN, LOW); 
+  }
+
+  mqttClient.loop();
+}
+
+/** Handle the response message */
+void handleResponse(char* topic, char* payload) {
+  Serial.print(topic);
+  Serial.print(": ");
+  Serial.println(payload);
+}
+
+/** Called when a response comes in from a subscribed topic */
+void callback(char* topic, byte* payload, unsigned int length) {
+  char* payloadCopy = (char*) malloc(length+1);
+  memcpy(payloadCopy, payload, length);
+  payloadCopy[length] = NULL;
+  handleResponse(topic, payloadCopy);
+  free(payloadCopy);
 }
