d74ceec8e7586e55e8cab18a40417c5504cafd00
Derek Adams <derek.adams@reveal-tech.com>
2014-02-02 22:45:26 -0500

    Updated protos to use different (lower memory) approach due to crashing problems. SITEWHERE-19

--- SiteWhere/sitewhere.c
+++ SiteWhere/sitewhere.c
@@ -2,20 +2,30 @@
 #include "sitewhere.pb.h"
 #include "pb_encode.h"
 
+// Signals end of stream.
+uint8_t zero = 0;
+
 boolean sw_register(char* hardwareId, char* specificationToken, char* replyTo, uint8_t* buffer, size_t length) {
-	sitewhere sw = { };
-	sw._command = sitewhere_Command_REGISTER;
-	sw.has_registerDevice = true;
-	sitewhere__type_registerDevice registerDevice = { };
+	pb_ostream_t stream = pb_ostream_from_buffer(buffer, length);
+
+	// Send header.
+	SiteWhere_Header header = {};
+	header.command = SiteWhere_Command_REGISTER;
+	if (!pb_encode_delimited(&stream, SiteWhere_Header_fields, &header)) {
+		return false;
+	}
+
+	// Send messsage.
+	SiteWhere_RegisterDevice registerDevice = {};
 	strcpy(registerDevice.hardwareId, hardwareId);
 	strcpy(registerDevice.specificationToken, specificationToken);
 	if (replyTo != NULL) {
 		registerDevice.has_replyTo = true;
 		strcpy(registerDevice.replyTo, replyTo);
 	}
+	if (!pb_encode_delimited(&stream, SiteWhere_RegisterDevice_fields, &registerDevice)) {
+		return false;
+	}
 
-	sw.registerDevice = registerDevice;
-
-	pb_ostream_t stream = pb_ostream_from_buffer(buffer, length);
-	return pb_encode(&stream, sitewhere_fields, &sw);
+	return pb_write(&stream, &zero, 1);
 }
