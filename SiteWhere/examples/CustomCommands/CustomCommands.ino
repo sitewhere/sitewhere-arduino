0a9528553e07d3be0b643a1dd25afafcf407758a
Derek Adams <derek.adams@sitewhere.com>
2016-09-27 10:34:39 -0400

    Updated CustomCommands example (tested with Mega2560).

--- SiteWhere/examples/CustomCommands/CustomCommands.ino
+++ SiteWhere/examples/CustomCommands/CustomCommands.ino
@@ -4,7 +4,7 @@
 #include <sitewhere.h>
 #include <sitewhere-arduino.pb.h>
 
-#include "custom.h"
+#include "custom.pb.h"
 
 #include <SPI.h>
 #include <Ethernet.h>
@@ -12,7 +12,7 @@
 
 // Update these with values suitable for your network.
 byte mac[]  = { 0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
-byte mqtt[] = { 192, 168, 1, 68 };
+byte mqtt[] = { 10, 0, 0, 32 };
 
 /** Callback function header */
 void callback(char* topic, byte* payload, unsigned int length);
@@ -34,16 +34,16 @@ static char* specificationToken = "417b36a8-21ef-4196-a8fe-cc756f994d0b";
 static char* outbound = "SiteWhere/input/protobuf";
 
 /** Inbound custom command topic */
-static char* command = "SiteWhere/commands/123-ARDUINO-TEST";
+static char* cmdtopic = "SiteWhere/commands/123-ARDUINO-TEST";
 
 /** Inbound system command topic */
-static char* system = "SiteWhere/system/123-ARDUINO-TEST";
+static char* systopic = "SiteWhere/system/123-ARDUINO-TEST";
 
 /** Handle MQTT subscription responses */
 void callback(char* topic, byte* payload, unsigned int length) {
-  if (strcmp(topic, system) == 0) {
+  if (strcmp(topic, systopic) == 0) {
     handleSystemCommand(payload, length);
-  } else if (strcmp(topic, command) == 0) {
+  } else if (strcmp(topic, cmdtopic) == 0) {
     handleSpecificationCommand(payload, length);
   }
 }
@@ -54,7 +54,8 @@ static void handleSystemCommand(byte* payload, unsigned int length) {
     pb_istream_t stream = pb_istream_from_buffer(payload, length);
 
     if (pb_decode_delimited(&stream, Device_Header_fields, &header)) {
-      if (header.command == Device_Command_REGISTER_ACK) {
+      // Handle a registration acknowledgement.
+      if (header.command == Device_Command_ACK_REGISTRATION) {
         Device_RegistrationAck ack;
         if (pb_decode_delimited(&stream, Device_RegistrationAck_fields, &ack)) {
           if (ack.state == Device_RegistrationAckState_NEW_REGISTRATION) {
@@ -73,27 +74,30 @@ static void handleSystemCommand(byte* payload, unsigned int length) {
 
 /** Handle a command specific to the specification for this device */
 static void handleSpecificationCommand(byte* payload, unsigned int length) {
-    ArduinoCustom__Header header;
-	pb_istream_t stream = pb_istream_from_buffer(payload, length);
-    if (pb_decode_delimited(&stream, ArduinoCustom__Header_fields, &header)) {
-      Serial.println("Decoded header for custom command.");
-      if (header.command == ArduinoCustom_Command_PING) {
-        ArduinoCustom_ping ping;
-        if (pb_decode_delimited(&stream, ArduinoCustom_ping_fields, &ping)) {
-          handlePing(ping, header.originator);
-        }
-      } else if (header.command == ArduinoCustom_Command_TESTEVENTS) {
-        ArduinoCustom_testEvents testEvents;
-        if (pb_decode_delimited(&stream, ArduinoCustom_testEvents_fields, &testEvents)) {
-          handleTestEvents(testEvents, header.originator);
-        }
-      } else if (header.command == ArduinoCustom_Command_SERIALPRINTLN) {
-        ArduinoCustom_serialPrintln serialPrintln;
-        if (pb_decode_delimited(&stream, ArduinoCustom_serialPrintln_fields, &serialPrintln)) {
-          handleSerialPrintln(serialPrintln, header.originator);
-        }
-      }
-    }
+  ArduinoCustom__Header header;
+  pb_istream_t stream = pb_istream_from_buffer(payload, length);
+  if (pb_decode_delimited(&stream, ArduinoCustom__Header_fields, &header)) {
+	Serial.println("Decoded header for custom command.");
+	if (header.command == ArduinoCustom_Command_PING) {
+	  Serial.println("Handling ping command...");
+	  ArduinoCustom_ping ping;
+	  if (pb_decode_delimited(&stream, ArduinoCustom_ping_fields, &ping)) {
+	    handlePing(ping, header.originator);
+	  }
+	} else if (header.command == ArduinoCustom_Command_TESTEVENTS) {
+	  Serial.println("Handling testEvents command...");
+	  ArduinoCustom_testEvents testEvents;
+	  if (pb_decode_delimited(&stream, ArduinoCustom_testEvents_fields, &testEvents)) {
+	    handleTestEvents(testEvents, header.originator);
+	  }
+	} else if (header.command == ArduinoCustom_Command_SERIALPRINTLN) {
+	  Serial.println("Handling serialPrintln command...");
+	  ArduinoCustom_serialPrintln serialPrintln;
+	  if (pb_decode_delimited(&stream, ArduinoCustom_serialPrintln_fields, &serialPrintln)) {
+	    handleSerialPrintln(serialPrintln, header.originator);
+	  }
+	}
+  }
 }
 
 /** Handle the 'ping' command */
@@ -137,8 +141,8 @@ void setup() {
   Serial.println(F("Connected to ethernet."));
   if (mqttClient.connect("arduinoClient")) {
     Serial.println(F("Connected to MQTT."));
-    mqttClient.subscribe(command);
-    mqttClient.subscribe(system);
+    mqttClient.subscribe(cmdtopic);
+    mqttClient.subscribe(systopic);
 
     if (sw_register(hardwareId, specificationToken, buffer, sizeof(buffer), NULL)) {
       mqttClient.publish(outbound, (char*) buffer);
--- SiteWhere/examples/CustomCommands/custom.pb
+++
@@ -1,22 +0,0 @@
-
-„
-custom.proto"ï
-ArduinoCustomn
-_Header'
-command (2.ArduinoCustom.Command
-
-originator (	
-
-nestedPath (	
-
-nestedSpec (	
-ping
-
-testEvents 
-serialPrintln
-message (	"6
-Command
-PING
-
-TESTEVENTS
-SERIALPRINTLNBH
